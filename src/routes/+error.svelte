de068f344ed2868a00d6c56e9f2eb483d3527e09
SurajSSingh <22825536+SurajSSingh@users.noreply.github.com>
2023-09-18 23:40:38 -0700

    feat: :sparkles: Add simple custom error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,18 @@
+<script>
+	// The ordering of these imports is critical to your app working properly
+	import '@skeletonlabs/skeleton/themes/theme-gold-nouveau.css';
+	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
+	import '@skeletonlabs/skeleton/styles/skeleton.css';
+	// Most of your app wide CSS should be put in this file
+	import '../app.postcss';
+</script>
+
+<div class="flex flex-col">
+	<h1 class="text-center">Error 404</h1>
+	<p class="text-center">This page does not exist.</p>
+	<p class="p-4 text-center">
+		<a href="/" class="p-4 underline variant-ghost-primary"
+			>Click here to go back to the homepage.</a
+		>
+	</p>
+</div>
